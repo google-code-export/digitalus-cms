a6166e9c8812d72d19f2ec5aff43070c01721390
forrestLyman <forrestLyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2008-11-17 03:14:11 +0000

    implemented wysiwg editor and markitup for wiki / css editor

--- application/contentTemplates/base/page/form.php
+++ application/contentTemplates/base/page/form.php
@@ -6,9 +6,11 @@ class Base_Page_Form extends DSF_Content_Form_Abstract {
             ->setAttrib('class', "med_short");
         
         $content = $this->form->createElement( 'textarea', 'content' );
+    
         $content->setRequired(true)
-            ->setLabel("Content:")
-            ->setAttrib('class',"med_tall");
+            ->setLabel('Content')
+            ->setDecorators(array('Composite'))
+            ->setAttrib('class',"editor wysiwyg");
         
         // Add elements to form:
         $this->form->addElement($teaser)
