2a66a600aec137e715e435048b5b5760c486949b
forrestLyman <forrestLyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2008-12-11 00:36:35 +0000

    added block folder content type

---
+++ trunk/application/contentTemplates/block/folder/form.php
@@ -0,0 +1,13 @@
+<?php
+class Block_Folder_Form extends DSF_Content_Form_Abstract {
+    public function setup() {        
+        $content = $this->form->createElement( 'textarea', 'content' );
+    
+        $content->setLabel('Description')
+            ->setAttrib('class',"med");
+        
+        // Add elements to form:
+        $this->form->addElement($content)
+            ->addElement('submit','update',array('label'=>'Update Page'));
+    }
+}
