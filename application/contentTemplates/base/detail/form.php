57c2839f85b5ea758892e0d7fe36f45e3cfa29d1
forrestLyman <forrestLyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2008-12-20 16:19:52 +0000

    updated all content forms to use html editor until we can resolve issue with jquery wysiwyg

--- application/contentTemplates/base/detail/form.php
+++ application/contentTemplates/base/detail/form.php
@@ -1,30 +1,21 @@
 <?php
-class Module_Page_Form extends DSF_Content_Form_Abstract 
+class Base_Detail_Form extends DSF_Content_Form_Abstract 
 {
-	public function setup()
-	{		
-		$teaser = $this->form->createElement('textarea','abstract');
-		$teaser->setLabel('Teaser:')
-			->setAttrib('class',"med_short");
-		
-		$contentAbove = $this->form->createElement('textarea', 'content_above');
-		$contentAbove->setRequired(false)
-			->setLabel("Content Above:")
-			->setAttrib('class',"med_short");
-			
-		$partial = $this->form->createElement("partial", "module", array("Partial" => "module/partials/load-module.phtml"));
-		$partial->setLabel("Select a module page");
-		
-		$contentBelow = $this->form->createElement('textarea', 'content_below');
-		$contentBelow->setRequired(false)
-			->setLabel("Content below:")
-			->setAttrib('class',"med_short");
-		
-		// Add elements to form:
-		$this->form->addElement($teaser)
-			->addElement($contentAbove)
-			->addElement($partial)
-			->addElement($contentBelow)
-			->addElement('submit', 'update', array('label' => 'Update Page'));
-	}
+    public function setup() {
+        $teaser = $this->form->createElement('textarea','teaser');
+        $teaser->setLabel('Teaser:')
+            ->setAttrib('class', "med_short");
+        
+        $content = $this->form->createElement( 'textarea', 'content' );
+    
+        $content->setRequired(true)
+            ->setLabel('Content')
+            ->setDecorators(array('Composite'))
+            ->setAttrib('class',"editor html");
+        
+        // Add elements to form:
+        $this->form->addElement($teaser)
+            ->addElement($content)
+            ->addElement('submit','update',array('label'=>'Update Page'));
+    }
 }
