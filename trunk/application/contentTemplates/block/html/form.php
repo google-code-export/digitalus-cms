b3f73cfa0a20ff89fdf7b189162b6f4f2a756cbb
lowtower@gmx.de <lowtower@gmx.de@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-02-12 11:43:52 +0000

    Added translation capability of modules; Added translation of mod_contact

--- trunk/application/contentTemplates/block/html/form.php
+++ trunk/application/contentTemplates/block/html/form.php
@@ -1,15 +1,17 @@
 <?php
 class Block_Html_Form extends DSF_Content_Form_Abstract {
-    public function setup() {        
-        $content = $this->form->createElement( 'textarea', 'content' );
-    
+    public function setup()
+    {
+        $view = $this->getView();
+        $content = $this->form->createElement('textarea', 'content');
+
         $content->setRequired(true)
-            ->setLabel('Content')
-            ->setDecorators(array('Composite'))
-            ->setAttrib('class',"editor html");
-        
+                ->setLabel($view->GetTranslation('Content'))
+                ->setDecorators(array('Composite'))
+                ->setAttrib('class', 'editor html');
+
         // Add elements to form:
         $this->form->addElement($content)
-            ->addElement('submit','update',array('label'=>'Update Page'));
+                   ->addElement('submit', 'update', array('label' => $view->GetTranslation('Update Page')));
     }
 }
