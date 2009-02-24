166968be917ae0d67bed86b60533eae9cd203ec8
forrestlyman <forrestlyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-02-24 08:36:27 +0000

    added RenderImage.php helper to general section

---
+++ trunk/application/helpers/general/RenderImage.php
@@ -0,0 +1,38 @@
+<?php
+class DSF_View_Helper_General_RenderImage
+{
+
+	/**
+	 * comments
+	 */
+	public function renderImage($src, $height, $width,$attribs = false){
+	    $absPath = SITE_ROOT . $src;
+		if($src != '' && file_exists($absPath)){
+		    
+    		$imageSize = getimagesize($absPath);
+    		$srcHeight = $imageSize[0];
+    		$srcWidth = $imageSize[1];
+
+    		//if the height is greater than the width then adjust by the height
+    		//otherwise adjust by the width
+    		if($srcHeight > $srcWidth){
+    			$percentage = $height / $srcHeight;
+    		}else{
+    			$percentage = $width / $srcWidth;
+    		}
+    		
+    		//gets the new value and applies the percentage, then rounds the value
+    		$width = round($srcWidth * $percentage);
+    		$height = round($srcHeight * $percentage);
+    		
+    		if($attribs){
+    			foreach ($attribs as $k => $v) {
+    				$attributes .= $k . "='" . $v . "' ";
+    			}
+    		}else{
+    		    $attributes = null;
+    		}
+    		return "<img width='{$width}' heigth='{$height}' src='{$src}' {$attributes}/>";
+		}
+	}
+}
