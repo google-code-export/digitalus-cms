36a710b3af2f34f7182a1e5e657adfeac265a18a
forrestlyman <forrestlyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-02-25 16:46:35 +0000

    updated DSF/Module.php so you can pass the getData() function content. it still defaults to the current page.

--- trunk/library/DSF/Module.php
+++ trunk/library/DSF/Module.php
@@ -1,22 +1,22 @@
 <?php
-
-class DSF_Module {
+class DSF_Module
+{
     const MODULE_KEY = 'module';
     protected $_page;
-
-    public function __construct() {
-       $this->_page = DSF_Builder::getPage();
-    }
-
-    public function getData()
+    public function __construct ()
     {
-        if (isset($this->_page)) {
+        $this->_page = DSF_Builder::getPage();
+    }
+    public function getData ($content = null)
+    {
+        if ($content == null && isset($this->_page)) {
             $content = $this->_page->getContent();
-            if (is_array($content) && isset($content[self::MODULE_KEY])) {
-                 return simplexml_load_string($content[self::MODULE_KEY]);
-            }
+        }
+        if (is_array($content) && isset($content[self::MODULE_KEY])) {
+            return simplexml_load_string($content[self::MODULE_KEY]);
+        } else {
+            return null;
         }
     }
 }
-
 ?>
