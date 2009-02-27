82d327dfcd24258cd2713f5a39dcbde79f9c4a6b
forrestlyman <forrestlyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-02-27 01:17:59 +0000

    added module block and wiki block content types

---
+++ trunk/application/contentTemplates/block/module/form.php
@@ -0,0 +1,29 @@
+<?php
+class Block_Module_Form extends DSF_Content_Form_Abstract
+{
+    public function setup()
+    {
+        $view = $this->getView();
+
+        $contentAbove = $this->form->createElement('textarea', 'content_above');
+        $contentAbove->setRequired(false)
+                     ->setLabel($view->GetTranslation('Content Above') . ':')
+                     ->setAttrib('cols', 40)
+                     ->setAttrib('rows', 8);
+
+        $partial = $this->form->createElement('partial', 'module', array('Partial' => 'module/partials/load-module.phtml'));
+        $partial->setLabel($view->GetTranslation('Select a module page'));
+
+        $contentBelow = $this->form->createElement('textarea', 'content_below');
+        $contentBelow->setRequired(false)
+                     ->setLabel($view->GetTranslation('Content below') . ':')
+                     ->setAttrib('cols', 40)
+                     ->setAttrib('rows', 8);
+
+        // Add elements to form:
+        $this->form->addElement($contentAbove)
+                   ->addElement($partial)
+                   ->addElement($contentBelow)
+                   ->addElement('submit', 'update', array('label' => $view->GetTranslation('Update Page')));
+    }
+}
