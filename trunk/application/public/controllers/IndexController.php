f39689c7abcb4bf7d141075a60221d60dc2ae516
forrestlyman <forrestlyman@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-03-08 02:19:59 +0000

    updated IndexController.php to load pages from cache if they are cached. if not it builds the page up to the step before the page object loads the view, then caches it. the back end caching reduces traffic on the db server, but does not speed up the page rendering substantially; it shaves off about 30%

--- trunk/application/public/controllers/IndexController.php
+++ trunk/application/public/controllers/IndexController.php
@@ -6,25 +6,43 @@
 class IndexController extends Zend_Controller_Action
 {
 	public $page;
+	public $cache;
+	public $pageGUID;
 	
 	public function init()
 	{
+	    // set up the cache and get the guid for the current page
+        $this->cache = Zend_Registry::get('cache');
+        $uri = new DSF_Uri();
+        $this->pageGUID = md5($uri->toString());
 	}
 	
-	public function indexAction()
-	{
-	    if(!$this->view->content) {	
-    		DSF_Builder::loadPage();
-    		$this->page = DSF_Builder::getPage();
-    		$this->view->page = $this->page;
-    		$this->view->layout()->page = $this->view->render('layouts/' . $this->page->getLayout());
-	    }
-	}
-	
-	public function renderPageAction()
-	{
-	    
-	}
+    public function indexAction()
+    {
+        $cachedPage = $this->cache->load($this->pageGUID);
+        
+        if($cachedPage) {
+           $this->page = $cachedPage;
+        }else{
+            // create the new page object
+            $this->page = DSF_Builder::loadPage(null, 'initialize.xml');
+            
+            // load the data
+            DSF_Builder::loadPage(null, 'load_data.xml', $this->page);
+            
+            // save it to the cache
+            $this->cache->save($this->page, $this->pageGUID);
+        }
+        
+        // load the view
+        DSF_Builder::loadPage(null, 'load_view.xml', $this->page, $this->view);
+        
+        // render the page
+        $this->view->page = $this->page;
+        $this->view->layout()->page = $this->view->render(
+        	'layouts/' . $this->page->getLayout()
+        );
+    }
 }
 
 
