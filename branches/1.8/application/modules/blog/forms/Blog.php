8bff75f4ab8c56b5bc8fc90c721b41760f8a56d1
lowtower@gmx.de <lowtower@gmx.de@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-06-04 21:56:23 +0000

    Third update of blog module

--- branches/1.8/application/modules/blog/forms/Blog.php
+++ branches/1.8/application/modules/blog/forms/Blog.php
@@ -4,12 +4,14 @@ class Blog_Form extends Zend_Form
     public function __construct($options = null) {
         parent::__construct($options);
 
+        $view = $this->getView();
+
         $id = $this->createElement('hidden', 'id');
 
         $name = $this->createElement('text', 'name');
-        $name->setAttrib('size',40);
-        $name->setRequired('true');
-        $name->setLabel('Blog Name: ');
+        $name->setAttrib('size', 40)
+             ->setRequired('true')
+             ->setLabel($view->getTranslation('Blog Name' . ':'));
 
         $submit = $this->createElement('submit', 'submit');
 
--- branches/1.8/application/modules/blog/forms/Post.php
+++ branches/1.8/application/modules/blog/forms/Post.php
@@ -4,20 +4,22 @@ class Post_Form extends Zend_Form
     public function __construct($options = null) {
         parent::__construct($options);
 
+        $view = $this->getView();
+
         $id = $this->createElement('hidden', 'id');
 
         $blog = $this->createElement('hidden', 'blog_id');
 
         $title = $this->createElement('text', 'title');
-        $title->setLabel('Title:');
+        $title->setLabel($view->getTranslation('Title') . ':');
 
         $teaser = $this->createElement('textarea', 'teaser');
-        $teaser->setLabel('Teaser:')
+        $teaser->setLabel($view->getTranslation('Teaser' . ':'))
                ->setAttrib('class', 'med_short');
 
         $content = $this->createElement('textarea', 'content');
         $content->setRequired(true)
-                ->setLabel('Content')
+                ->setLabel($view->getTranslation('Content'))
                 ->setAttrib('class', 'editor wysiwyg');
 
         $submit = $this->createElement('submit', 'submit');
