d5532cb7662894a582fc1c3382875b3c33260493
lowtower@gmx.de <lowtower@gmx.de@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-10-06 15:38:51 +0000

    Added phpdoc description to forms

--- tags/1.9/application/admin/forms/Login.php
+++ tags/1.9/application/admin/forms/Login.php
@@ -1,6 +1,47 @@
 <?php
+/**
+ * Digitalus CMS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://digitalus-media.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@digitalus-media.com so we can send you a copy immediately.
+ *
+ * @copyright   Copyright (c) 2007 - 2009,  Digitalus Media USA (digitalus-media.com)
+ * @license     http://digitalus-media.com/license/new-bsd     New BSD License
+ * @version     $Id:$
+ * @link        http://www.digitaluscms.com
+ * @since       Release 1.9.0
+ */
+
+/**
+ * @see Digitalus_Form
+ */
+require_once 'Digitalus/Form.php';
+
+/**
+ * Admin Login Form
+ *
+ * @copyright   Copyright (c) 2007 - 2009,  Digitalus Media USA (digitalus-media.com)
+ * @license     http://digitalus-media.com/license/new-bsd     New BSD License
+ * @category    Digitalus CMS
+ * @package     Digitalus_CMS_Admin
+ * @version     $Id:$
+ * @link        http://www.digitaluscms.com
+ * @since       Release 1.9.0
+ */
 class Admin_Form_Login extends Digitalus_Form
 {
+    /**
+     * Initialize the form
+     *
+     * @return void
+     */
     public function init()
     {
         $this->setAction($this->getView()->getBaseUrl() . '/admin/auth/login')
