68c9f112924c3744c13b5cd853e02d497f534095
lowtower@gmx.de <lowtower@gmx.de@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2009-12-02 16:46:13 +0000

    Remove bug in installation process

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
+// Define base path obtainable throughout the whole application
+defined('BASE_PATH')
+    || define('BASE_PATH', realpath(dirname(__FILE__)));
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', BASE_PATH . '/application');
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// check whether we are in installation process or not
 if (file_exists('./install/install.php')) {
     // Start installation if the file "install.php" exists in the directory "install"
     require './install/install.php';
 } else {
-    // Define base path obtainable throughout the whole application
-    defined('BASE_PATH')
-        || define('BASE_PATH', realpath(dirname(__FILE__)));
-    // Define path to application directory
-    defined('APPLICATION_PATH')
-        || define('APPLICATION_PATH', BASE_PATH . '/application');
-    // Define application environment
-    defined('APPLICATION_ENV')
-        || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
     // Set include path to Zend (and other) libraries
     set_include_path(BASE_PATH . '/library' .
         PATH_SEPARATOR . APPLICATION_PATH . '/models' .
