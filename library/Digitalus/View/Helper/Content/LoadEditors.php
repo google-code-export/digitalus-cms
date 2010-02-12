1bf7909f94642949c8670dc57f397ff5e872cae7
lowtower@gmx.de <lowtower@gmx.de@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2010-02-12 09:59:32 +0000

    Upgrade of HTML editors to current versions - library changes

--- library/Digitalus/View/Helper/Content/LoadEditors.php
+++ library/Digitalus/View/Helper/Content/LoadEditors.php
@@ -44,8 +44,12 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
 
     protected $_editorTypes = array();
 
+    protected $_scriptsPath = '';
+
     public function loadEditors($controls)
     {
+        $this->_scriptsPath = $this->view->getBaseUrl() . '/scripts';
+
         $adminLanguage = $this->view->getAdminLanguage();
 
         $this->_editorTypes = $this->_getEditorTypes($controls);
@@ -55,7 +59,8 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
                 switch (strtolower($editor)) {
                     case 'ckeditor':
                         // integrates CkEditor
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/ckeditor/ckeditor.js');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/ckeditor/ckeditor.js');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/ckeditor/adapters/jquery.js');
                         $this->view->jQuery()->onLoadCaptureStart();?>
                             $('.ckeditor').ckeditor({
                                 language : '<?php echo $adminLanguage;?>',
@@ -65,10 +70,10 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
                     case 'fckeditor':
                     default:
                         // integrates FckEditor
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/fckeditor/jquery.FCKEditor.pack.js');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/fckeditor/jquery.FCKEditor.pack.js');
                         $this->view->jQuery()->onLoadCaptureStart();?>
                             $('.fckeditor').fck({
-                                path: '<?php echo $this->view->getBaseUrl() . '/scripts/fckeditor/';?>',
+                                path: '<?php echo $this->_scriptsPath . '/fckeditor/';?>',
                                 toolbar: 'Digitalus',
                                 height: 300
                             });<?php
@@ -76,10 +81,10 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
                         break;
                     case 'markitup':
                         // integrates MarkItUp
-                        $this->view->jQuery()->addStylesheet($this->view->getBaseUrl() . '/scripts/markitup/skins/markitup/style.css');
-                        $this->view->jQuery()->addStylesheet($this->view->getBaseUrl() . '/scripts/markitup/sets/html/style.css');
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/markitup/jquery.markitup.js');
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/markitup/sets/html/set.js');
+                        $this->view->jQuery()->addStylesheet($this->_scriptsPath . '/markitup/skins/markitup/style.css');
+                        $this->view->jQuery()->addStylesheet($this->_scriptsPath . '/markitup/sets/html/style.css');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/markitup/jquery.markitup.js');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/markitup/sets/html/set.js');
                         $this->view->jQuery()->onLoadCaptureStart();?>
                             $('.markItUp').markItUp(
                                 mySettings
@@ -88,18 +93,18 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
                         break;
                     case 'tinymce':
                         // integrates TinyMce
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/tinymce/jquery.tinymce.js');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/tiny_mce/jquery.tinymce.js');
                         $this->view->jQuery()->onLoadCaptureStart();?>
                             jQuery('.tinymce').tinymce({
                                 lang: '<?php echo $adminLanguage;?>',
                                 theme: 'advanced',
-                                script_url: '<?php echo $this->view->getBaseUrl() . '/scripts/tinymce/tiny_mce.js';?>',
+                                script_url: '<?php echo $this->_scriptsPath . '/tiny_mce/tiny_mce.js';?>',
                             });<?php
                         $this->view->jQuery()->onLoadCaptureEnd();
                         break;
                     case 'wymeditor':
                         // integrates WymEditor
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/wymeditor/jquery.wymeditor.min.js');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/wymeditor/jquery.wymeditor.min.js');
                         $this->view->jQuery()->onLoadCaptureStart();?>
                             jQuery('.wymeditor').wymeditor({
                                 lang: '<?php echo $adminLanguage;?>',
@@ -116,8 +121,8 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
                         break;
                     case 'wysiwyg':
                         // integrates Jwysiwyg
-                        $this->view->jQuery()->addStylesheet($this->view->getBaseUrl() . '/scripts/jquery/plugins/wysiwyg/jquery.wysiwyg.css');
-                        $this->view->jQuery()->addJavascriptFile($this->view->getBaseUrl() . '/scripts/jquery/plugins/wysiwyg/jquery.wysiwyg.js');
+                        $this->view->jQuery()->addStylesheet($this->_scriptsPath . '/jquery/plugins/jwysiwyg/jquery.wysiwyg.css');
+                        $this->view->jQuery()->addJavascriptFile($this->_scriptsPath . '/jquery/plugins/jwysiwyg/jquery.wysiwyg.js');
                         $this->view->jQuery()->onLoadCaptureStart();?>
                             $('.wysiwyg').wysiwyg({
                                 controls: {
@@ -142,5 +147,4 @@ class Digitalus_View_Helper_Content_LoadEditors extends Zend_View_Helper_Abstrac
 
         return $this->_editorTypes;
     }
-
 }
