70c15eaecfdb4fe0e32c2df89cc19ca73ac60f2b
lowtower@gmx.de <lowtower@gmx.de@3725d64e-a86f-11dd-a887-7ffe1a420f8d>
2011-01-04 17:06:21 +0000

    fixes issue 164

--- trunk/application/modules/search/adapter/Page.php
+++ trunk/application/modules/search/adapter/Page.php
@@ -21,11 +21,11 @@ class Search_Adapter_Page
     public function asLuceneDocument()
     {
         $doc = new Zend_Search_Lucene_Document();
-        $doc->addField(Zend_Search_Lucene_Field::Text('page_title', $this->title, $this->_charset));
-        $doc->addField(Zend_Search_Lucene_Field::Text('page_link', $this->path, $this->_charset));
-        $doc->addField(Zend_Search_Lucene_Field::Text('page_teaser', $this->teaser, $this->_charset));
-        $doc->addField(Zend_Search_Lucene_Field::unstored('page_content', $this->content, $this->_charset));
-        $doc->addField(Zend_Search_Lucene_Field::UnStored('search_tags', $this->searchTags, $this->_charset));
+        $doc->addField(Zend_Search_Lucene_Field::Text('page_title',       $this->title,      $this->_charset));
+        $doc->addField(Zend_Search_Lucene_Field::Text('page_link',        $this->path,       $this->_charset));
+        $doc->addField(Zend_Search_Lucene_Field::Text('page_teaser',      $this->teaser,     $this->_charset));
+        $doc->addField(Zend_Search_Lucene_Field::unstored('page_content', $this->content,    $this->_charset));
+        $doc->addField(Zend_Search_Lucene_Field::UnStored('search_tags',  $this->searchTags, $this->_charset));
         return $doc;
     }
 
